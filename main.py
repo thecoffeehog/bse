470431833d7b360037547481830baf0a8467038c
Udit Chugh <uditvapi69@gmail.com>
2018-06-03 13:00:49 +0530

    Added provison to get top 10 stocks using the sorted sets data structure

--- main.py
+++ main.py
@@ -15,16 +15,17 @@ class StringGenerator(object):
 
         # Download Zip TODO: Make Link using date
         url = 'https://www.bseindia.com/download/BhavCopy/Equity/EQ010618_CSV.ZIP'
-        r = requests.get(url)
+        request = requests.get(url)
 
         # Extract Zip
-        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z = zipfile.ZipFile(io.BytesIO(request.content))
         z.extractall()
 
         # Save CSV into the Redis DB
         with open('EQ010618.csv', 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
-
+            red.flushall()
+            top_ten = ''
             for row in csv_reader:
                 red.hset('code:' + row['SC_CODE'], 'code', row['SC_CODE'])
                 red.hset('code:' + row['SC_CODE'], 'name', row['SC_NAME'].strip())
@@ -32,8 +33,16 @@ class StringGenerator(object):
                 red.hset('code:' + row['SC_CODE'], 'high', row['HIGH'])
                 red.hset('code:' + row['SC_CODE'], 'low', row['LOW'])
                 red.hset('code:' + row['SC_CODE'], 'close', row['CLOSE'])
+                # red.zadd('open', 'code:' + row['SC_CODE'], row['OPEN'])
+                red.zadd('open',  row['OPEN'], 'code:' + row['SC_CODE'])
 
-        print(red.hgetall('code:500002'))
+                top_ten = red.zrevrange('open', 0, 9)
+
+        print(top_ten)
+
+        print('Top 10 Stocks!')
+        for stock in top_ten:
+            print(red.hgetall(stock))
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '127.0.0.1','server.socket_port': 8080})

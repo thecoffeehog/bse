d91fe5f8c1b6adf7e35bafe99e5b1874de3c087a
Udit Chugh <uditvapi69@gmail.com>
2018-06-05 00:24:08 +0530

    Refactored Some Code

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ red = redis.StrictRedis(host='localhost', port=6379,
 
 
 class BSE(object):
+
     showing_data_for = ''
 
     @cherrypy.expose
     def index(self):
-        self.store_data_in_redis()
-        top_ten = self.get_top_ten_stocks()
 
+        csv_data, date_of_csv = self.get_csv()
+
+        self.store_csv_in_redis(csv_data, date_of_csv)
+
+        return self.get_html(self.get_top_ten_stocks())
+
+    def get_html(self,top_ten):
         html_mid = ''
 
         for stock in top_ten:
@@ -37,52 +43,54 @@ class BSE(object):
                        open_html + high_html + low_html + close_html + end_r
 
         html_front = """
-            <html>
-            <head>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-                <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-                <link rel="stylesheet" href="/static/css/main.css">
-                <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-            </head>
-            <body>
-            <div id=header align=center>
-            <img width="100" height="34.5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Bombay_Stock_Exchange_logo.svg/2000px-Bombay_Stock_Exchange_logo.svg.png">
-            Top 10 stocks on """ + str(self.showing_data_for.date().strftime('%A %d %B %Y')) + """
-            </div>
-            <table align="center" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
-                <thead>
-                <tr>
-                    <th class="mdl-data-table__cell--non-numeric">Code</th>
-                    <th class="mdl-data-table__cell--non-numeric">Name</th>
-                    <th>Open</th>
-                    <th>High</th>
-                    <th>Low</th>
-                    <th>Close</th>
-                </tr>"""
-        html_rear = """
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
-            <div id="info" align="center">
-            Use the below button to search details of any other BSE stock by its name like HDFC or DR.REDDY'S  
-            <form action="search">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-                     <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
-                     <i class="material-icons">search</i>
-                     </label>
-                    <div class="mdl-textfield__expandable-holder">
-                        <input class="mdl-textfield__input" type="text" name="name_of_stock" id="search">
-                        <label class="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+                    <html>
+                    <head>
+                        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+                        <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+                        <link rel="stylesheet" href="/static/css/main.css">
+                        <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+                    </head>
+                    <body>
+                    <div id=header align=center>
+                    <img width="100" height="34.5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Bombay_Stock_Exchange_logo.svg/2000px-Bombay_Stock_Exchange_logo.svg.png">
+                    Top 10 stocks on """ + str(self.showing_data_for.date().strftime('%A %d %B %Y')) + """
                     </div>
-                </div>
-            </form>
-            </div>
+                    <table align="center" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                        <thead>
+                        <tr>
+                            <th class="mdl-data-table__cell--non-numeric">Code</th>
+                            <th class="mdl-data-table__cell--non-numeric">Name</th>
+                            <th>Open</th>
+                            <th>High</th>
+                            <th>Low</th>
+                            <th>Close</th>
+                        </tr>"""
+        html_rear = """
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                    <div id="info" align="center">
+                    Use the below button to search details of any other BSE stock by its name like HDFC or DR.REDDY'S  
+                    <form action="search">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                             <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
+                             <i class="material-icons">search</i>
+                             </label>
+                            <div class="mdl-textfield__expandable-holder">
+                                <input class="mdl-textfield__input" type="text" name="name_of_stock" id="search">
+                                <label class="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
 
-            </body>
-            </html>
-            """
+                    </body>
+                    </html>
+                    """
         return html_front + html_mid + html_rear
+
+
 
     @cherrypy.expose
     def search(self, name_of_stock):
@@ -144,7 +152,29 @@ class BSE(object):
 
         return html_front + html_mid + html_rear
 
-    def store_data_in_redis(self):
+    def store_csv_in_redis(self, csv_path, date_of_csv):
+        # Save CSV into the Redis DB
+
+        with open(csv_path, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            self.showing_data_for = date_of_csv
+
+            # Store the data we need from the CSV to the Redis DB
+
+            for row in csv_reader:
+                red.hset('code:' + row['SC_CODE'], 'code', row['SC_CODE'])
+                # Because CSV returns name w/ spaces
+                red.hset('code:' + row['SC_CODE'],
+                         'name', row['SC_NAME'].strip())
+                red.hset('code:' + row['SC_CODE'], 'open', row['OPEN'])
+                red.hset('code:' + row['SC_CODE'], 'high', row['HIGH'])
+                red.hset('code:' + row['SC_CODE'], 'low', row['LOW'])
+                red.hset('code:' + row['SC_CODE'], 'close', row['CLOSE'])
+
+                red.zadd('open', row['OPEN'], 'code:' + row['SC_CODE'])  # To find top 10 stocks quickly
+                red.zadd('name', row['SC_CODE'], row['SC_NAME'].strip())  # To find by name
+
+    def get_csv(self):
 
         red.flushall()
 
@@ -152,10 +182,16 @@ class BSE(object):
         # go and try to find one from previous day and keep trying for previous ten days
         i = 0
         while i < 10:
-            d = datetime.datetime.today() - datetime.timedelta(days=i)
-            print(d)
-            date_str = str('%02d' % d.day) + str('%02d' % d.month) + str(d.year - 2000)
+
+            date_of_csv = datetime.datetime.today() - datetime.timedelta(days=i)
+            print(date_of_csv)
+            date_str = str('%02d' % date_of_csv.day) + str('%02d' % date_of_csv.month) + str(date_of_csv.year - 2000)
             csv_path = 'EQ' + date_str + '.CSV'
+
+            if os.path.exists('./'+csv_path):
+                print('path exists')
+                return csv_path, date_of_csv
+
             url = 'https://www.bseindia.com/download/BhavCopy/Equity/EQ' + date_str + '_CSV.ZIP'
             request = requests.get(url)
 
@@ -172,28 +208,7 @@ class BSE(object):
                 # Extract Zip
                 z = zipfile.ZipFile(io.BytesIO(request.content))
                 z.extractall()
-
-                # Save CSV into the Redis DB
-
-                with open(csv_path, 'r') as csv_file:
-                    csv_reader = csv.DictReader(csv_file)
-                    self.showing_data_for = d
-
-                    # Store the data we need from the CSV to the Redis DB
-
-                    for row in csv_reader:
-                        red.hset('code:' + row['SC_CODE'], 'code', row['SC_CODE'])
-                        # Because CSV returns name w/ spaces
-                        red.hset('code:' + row['SC_CODE'],
-                                 'name', row['SC_NAME'].strip())
-                        red.hset('code:' + row['SC_CODE'], 'open', row['OPEN'])
-                        red.hset('code:' + row['SC_CODE'], 'high', row['HIGH'])
-                        red.hset('code:' + row['SC_CODE'], 'low', row['LOW'])
-                        red.hset('code:' + row['SC_CODE'], 'close', row['CLOSE'])
-
-                        red.zadd('open', row['OPEN'], 'code:' + row['SC_CODE'])  # To find top 10 stocks quickly
-                        red.zadd('name', row['SC_CODE'], row['SC_NAME'].strip())  # To find by name
-                return
+                return csv_path, date_of_csv
 
             else:
                 i += 1

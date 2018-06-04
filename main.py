76fe3a7844df6e4c0ce2f061b125e0c317ed110c
Udit Chugh <uditvapi69@gmail.com>
2018-06-04 23:53:52 +0530

    Getting the CSV file based on current date

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import io
 import csv
 import redis
 import os
+import datetime
 
 red = redis.StrictRedis(host='localhost', port=6379,
                         db=0, decode_responses=True)
 
 
 class BSE(object):
+    showing_data_for = ''
+
     @cherrypy.expose
     def index(self):
         self.store_data_in_redis()
@@ -22,7 +25,7 @@ class BSE(object):
             stock_info = red.hgetall(stock)
             code_html = '<td>' + stock_info['code'] + '</td>'
             name_html = '<td class="mdl-data-table__cell--non-numeric">' + \
-                stock_info['name'] + '</td>'
+                        stock_info['name'] + '</td>'
             open_html = '<td>' + stock_info['open'] + '</td>'
             high_html = '<td>' + stock_info['high'] + '</td>'
             low_html = '<td>' + stock_info['low'] + '</td>'
@@ -31,7 +34,7 @@ class BSE(object):
             end_r = '</tr>'
 
             html_mid = html_mid + begin_r + code_html + name_html + \
-                open_html + high_html + low_html + close_html + end_r
+                       open_html + high_html + low_html + close_html + end_r
 
         html_front = """
             <html>
@@ -44,7 +47,7 @@ class BSE(object):
             <body>
             <div id=header align=center>
             <img width="100" height="34.5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Bombay_Stock_Exchange_logo.svg/2000px-Bombay_Stock_Exchange_logo.svg.png">
-            Top 10 stocks of the day
+            Top 10 stocks on """ + str(self.showing_data_for.date().strftime('%A %d %B %Y')) + """
             </div>
             <table align="center" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
                 <thead>
@@ -62,7 +65,7 @@ class BSE(object):
                 </tbody>
             </table>
             <div id="info" align="center">
-            Search other BSE stocks by Name using the below button
+            Use the below button to search details of any other BSE stock by its name like HDFC or DR.REDDY'S  
             <form action="search">
                 <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
                      <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
@@ -80,28 +83,30 @@ class BSE(object):
             </html>
             """
         return html_front + html_mid + html_rear
-        
+
     @cherrypy.expose
-    def search(self,name_of_stock):
+    def search(self, name_of_stock):
+
+        name_of_stock = (name_of_stock.strip()).upper()
+
         stock_code = red.zscore('name', name_of_stock)
+
         if not stock_code:
             return 'Please enter a valid stock name and try again.'
+
         stock_hash_key = 'code:' + str(int(stock_code))
+
         stock_info = red.hgetall(stock_hash_key)
 
-        
         code_html = '<td>' + stock_info['code'] + '</td>'
         name_html = '<td class="mdl-data-table__cell--non-numeric">' + \
-            stock_info['name'] + '</td>'
+                    stock_info['name'] + '</td>'
         open_html = '<td>' + stock_info['open'] + '</td>'
         high_html = '<td>' + stock_info['high'] + '</td>'
         low_html = '<td>' + stock_info['low'] + '</td>'
         close_html = '<td>' + stock_info['close'] + '</td>'
         begin_r = '<tr>'
         end_r = '</tr>'
-
-        html_mid = begin_r + code_html + name_html + \
-                open_html + high_html + low_html + close_html + end_r
 
         html_front = """
             <html>
@@ -125,6 +130,9 @@ class BSE(object):
                     <th>Low</th>
                     <th>Close</th>
                 </tr>"""
+
+        html_mid = begin_r + code_html + name_html + open_html + high_html + low_html + close_html + end_r
+
         html_rear = """
                 </thead>
                 <tbody>
@@ -138,35 +146,57 @@ class BSE(object):
 
     def store_data_in_redis(self):
 
-        # Download Zip TODO: Make Link using date
-        url = 'https://www.bseindia.com/download/BhavCopy/Equity/EQ010618_CSV.ZIP'
-        request = requests.get(url)
+        red.flushall()
 
-        # Extract Zip
-        z = zipfile.ZipFile(io.BytesIO(request.content))
-        z.extractall()
+        # Try to find the zip for the current day, if not found
+        # go and try to find one from previous day and keep trying for previous ten days
+        i = 0
+        while i < 10:
+            d = datetime.datetime.today() - datetime.timedelta(days=i)
+            print(d)
+            date_str = str('%02d' % d.day) + str('%02d' % d.month) + str(d.year - 2000)
+            csv_path = 'EQ' + date_str + '.CSV'
+            url = 'https://www.bseindia.com/download/BhavCopy/Equity/EQ' + date_str + '_CSV.ZIP'
+            request = requests.get(url)
 
-        # Save CSV into the Redis DB
-        csv_path='EQ010618.CSV'
-        with open(csv_path, 'r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            red.flushall()
+            if request.status_code == 200:
 
-            # Store the data we need from the CSV to the Redis DB
+                #Delete old CSV files, if any
 
-            for row in csv_reader:
-                red.hset('code:' + row['SC_CODE'], 'code', row['SC_CODE'])
-                # Because CSV returns name w/ spaces
-                red.hset('code:' + row['SC_CODE'],
-                         'name', row['SC_NAME'].strip())
-                red.hset('code:' + row['SC_CODE'], 'open', row['OPEN'])
-                red.hset('code:' + row['SC_CODE'], 'high', row['HIGH'])
-                red.hset('code:' + row['SC_CODE'], 'low', row['LOW'])
-                red.hset('code:' + row['SC_CODE'], 'close', row['CLOSE'])
+                cwd = os.listdir(os.getcwd())
 
-                red.zadd('open', row['OPEN'], 'code:' + row['SC_CODE']) # To find top 10 stocks quickly
-                red.zadd('name', row['SC_CODE'], row['SC_NAME'].strip()) # To find by name
-            
+                for item in cwd:
+                    if item.endswith(".CSV"):
+                        os.remove(os.path.join(os.getcwd(), item))
+
+                # Extract Zip
+                z = zipfile.ZipFile(io.BytesIO(request.content))
+                z.extractall()
+
+                # Save CSV into the Redis DB
+
+                with open(csv_path, 'r') as csv_file:
+                    csv_reader = csv.DictReader(csv_file)
+                    self.showing_data_for = d
+
+                    # Store the data we need from the CSV to the Redis DB
+
+                    for row in csv_reader:
+                        red.hset('code:' + row['SC_CODE'], 'code', row['SC_CODE'])
+                        # Because CSV returns name w/ spaces
+                        red.hset('code:' + row['SC_CODE'],
+                                 'name', row['SC_NAME'].strip())
+                        red.hset('code:' + row['SC_CODE'], 'open', row['OPEN'])
+                        red.hset('code:' + row['SC_CODE'], 'high', row['HIGH'])
+                        red.hset('code:' + row['SC_CODE'], 'low', row['LOW'])
+                        red.hset('code:' + row['SC_CODE'], 'close', row['CLOSE'])
+
+                        red.zadd('open', row['OPEN'], 'code:' + row['SC_CODE'])  # To find top 10 stocks quickly
+                        red.zadd('name', row['SC_CODE'], row['SC_NAME'].strip())  # To find by name
+                return
+
+            else:
+                i += 1
 
     def get_top_ten_stocks(self):
         return red.zrevrange('open', 0, 9)
@@ -186,4 +216,3 @@ if __name__ == '__main__':
         }
     }
     cherrypy.quickstart(BSE(), '/', conf)
-

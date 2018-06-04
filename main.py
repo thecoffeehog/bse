4e665852baa2f360caee8c85db26f5ba892567b0
Ubuntu <ubuntu@ip-172-31-17-0.us-west-2.compute.internal>
2018-06-04 13:39:07 +0000

    Added the ability to search any BSE listed stock present in the  CSV file

--- main.py
+++ main.py
@@ -13,18 +13,20 @@ red = redis.StrictRedis(host='localhost', port=6379,
 class BSE(object):
     @cherrypy.expose
     def index(self):
-
+        self.store_data_in_redis()
         top_ten = self.get_top_ten_stocks()
 
         html_mid = ''
+
         for stock in top_ten:
-            code_html = '<td>' + red.hgetall(stock)['code'] + '</td>'
+            stock_info = red.hgetall(stock)
+            code_html = '<td>' + stock_info['code'] + '</td>'
             name_html = '<td class="mdl-data-table__cell--non-numeric">' + \
-                red.hgetall(stock)['name'] + '</td>'
-            open_html = '<td>' + red.hgetall(stock)['open'] + '</td>'
-            high_html = '<td>' + red.hgetall(stock)['high'] + '</td>'
-            low_html = '<td>' + red.hgetall(stock)['low'] + '</td>'
-            close_html = '<td>' + red.hgetall(stock)['close'] + '</td>'
+                stock_info['name'] + '</td>'
+            open_html = '<td>' + stock_info['open'] + '</td>'
+            high_html = '<td>' + stock_info['high'] + '</td>'
+            low_html = '<td>' + stock_info['low'] + '</td>'
+            close_html = '<td>' + stock_info['close'] + '</td>'
             begin_r = '<tr>'
             end_r = '</tr>'
 
@@ -61,13 +63,13 @@ class BSE(object):
             </table>
             <div id="info" align="center">
             Search other BSE stocks by Name using the below button
-            <form action="#">
+            <form action="search">
                 <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
                      <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
                      <i class="material-icons">search</i>
                      </label>
                     <div class="mdl-textfield__expandable-holder">
-                        <input class="mdl-textfield__input" type="text" id="search">
+                        <input class="mdl-textfield__input" type="text" name="name_of_stock" id="search">
                         <label class="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
                     </div>
                 </div>
@@ -78,8 +80,63 @@ class BSE(object):
             </html>
             """
         return html_front + html_mid + html_rear
+        
+    @cherrypy.expose
+    def search(self,name_of_stock):
+        stock_code = red.zscore('name', name_of_stock)
+        if not stock_code:
+            return 'Please enter a valid stock name and try again.'
+        stock_hash_key = 'code:' + str(int(stock_code))
+        stock_info = red.hgetall(stock_hash_key)
 
-    def get_top_ten_stocks(self):
+        
+        code_html = '<td>' + stock_info['code'] + '</td>'
+        name_html = '<td class="mdl-data-table__cell--non-numeric">' + \
+            stock_info['name'] + '</td>'
+        open_html = '<td>' + stock_info['open'] + '</td>'
+        high_html = '<td>' + stock_info['high'] + '</td>'
+        low_html = '<td>' + stock_info['low'] + '</td>'
+        close_html = '<td>' + stock_info['close'] + '</td>'
+        begin_r = '<tr>'
+        end_r = '</tr>'
+
+        html_mid = begin_r + code_html + name_html + \
+                open_html + high_html + low_html + close_html + end_r
+
+        html_front = """
+            <html>
+            <head>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+                <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+                <link rel="stylesheet" href="/static/css/main.css">
+                <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+            </head>
+            <body>
+            <div id=header align=center>
+            <img width="100" height="34.5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Bombay_Stock_Exchange_logo.svg/2000px-Bombay_Stock_Exchange_logo.svg.png">
+            </div>
+            <table align="center" class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+                <thead>
+                <tr>
+                    <th class="mdl-data-table__cell--non-numeric">Code</th>
+                    <th class="mdl-data-table__cell--non-numeric">Name</th>
+                    <th>Open</th>
+                    <th>High</th>
+                    <th>Low</th>
+                    <th>Close</th>
+                </tr>"""
+        html_rear = """
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+            </body>
+            </html>
+            """
+
+        return html_front + html_mid + html_rear
+
+    def store_data_in_redis(self):
 
         # Download Zip TODO: Make Link using date
         url = 'https://www.bseindia.com/download/BhavCopy/Equity/EQ010618_CSV.ZIP'
@@ -94,7 +151,6 @@ class BSE(object):
         with open(csv_path, 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             red.flushall()
-            top_ten = ''
 
             # Store the data we need from the CSV to the Redis DB
 
@@ -108,12 +164,12 @@ class BSE(object):
                 red.hset('code:' + row['SC_CODE'], 'low', row['LOW'])
                 red.hset('code:' + row['SC_CODE'], 'close', row['CLOSE'])
 
-                red.zadd('open', row['OPEN'], 'code:' + row['SC_CODE'])
-                #red.zadd('name', row['SC_CODE'], row['SC_NAME'].strip())
+                red.zadd('open', row['OPEN'], 'code:' + row['SC_CODE']) # To find top 10 stocks quickly
+                red.zadd('name', row['SC_CODE'], row['SC_NAME'].strip()) # To find by name
+            
 
-                top_ten = red.zrevrange('open', 0, 9)
-
-        return top_ten
+    def get_top_ten_stocks(self):
+        return red.zrevrange('open', 0, 9)
 
 
 if __name__ == '__main__':
@@ -130,4 +186,4 @@ if __name__ == '__main__':
         }
     }
     cherrypy.quickstart(BSE(), '/', conf)
-t
+
